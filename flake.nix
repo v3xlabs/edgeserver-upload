85b46552d4a46c49e5573517b2b92c5a7834c928
Luc <luc@lucemans.nl>
2026-06-19 12:59:20 +0200

    Update node 24

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "openlv devshell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          just
+          nodejs_24
+          pnpm_11
+        ];
+
+        shellHook = ''
+          just
+        '';
+      };
+    });
+}
